ab6a153e11ee205f505192569a9c467504f5cd0d
ccw <cwc1002s@gmail.com>
2023-05-11 22:50:22 +0900

    add clock

--- js/greetings.js
+++ js/greetings.js
@@ -14,8 +14,8 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
+  greeting.innerText = `Hello, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.textContent = `Hello, ${username}!`;
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
